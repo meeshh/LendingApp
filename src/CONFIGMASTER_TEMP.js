fc0af254201cd1968f7cdcb57160e7efc3f8049b
meeshh <mick.f4rr3ll@gmail.com>
2018-09-06 21:28:29 +0300

    Added firebase config file template

---
+++ src/CONFIGMASTER_TEMP.js
@@ -0,0 +1,12 @@
+//go to firebase.google.com, create a project
+//you can find its settings in your firebase console.
+//replace these settings with your real ones and rename this file to CONFIGMASTER.js (basically remove the _TEMP)
+
+export const firebaseConfig = {
+	apiKey: "YOUR_FIREBASER_API_KEY",
+	authDomain: "YOUR_FIREBASE_AUTH_DOMAIN",
+	databaseURL: "YOUR_FIREBASE_DATABASE_URL",
+	projectId: "YOUR PROJECT ID",
+	storageBucket: "",
+	messagingSenderId: "YOUR MESSAGING CENTER ID"
+};
